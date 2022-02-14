0f521407efd1062023ae43519e853867fff77b53
unknown <abegundeosamuel@gmail.com>
2022-02-14 14:38:22 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="home-App">
+      <div className='filter_container-parent'>
+        Filter Container
+      </div>
+      <div className='products_container-parent'>
+         Edvora
+      </div>
+    </div>
+  );
+}
+
+export default App;
